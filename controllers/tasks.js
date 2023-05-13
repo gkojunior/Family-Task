99e0320ffee21d5016ff2d2409a900e27f6e3e51
georgeowusu <gkojunior@gmail.com>
2023-05-13 17:37:48 -0400

    fixing deploy issue

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,4 +1,4 @@
-const Task = require('../models/Task')
+const Task = require('../models/task')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
